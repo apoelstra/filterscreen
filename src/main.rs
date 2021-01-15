bc599135e521d008659018572cb51d54fc964aa8
Andrew Poelstra <apoelstra@wpsoftware.net>
2021-01-15 20:41:47 +0000

    more URL characters

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() -> io::Result<()> {
     let mut alnum_state = false;
     for byte in io::stdin().bytes() {
         let byte = byte?;
-        let new_alnum_state = (byte as char).is_alphanumeric() || byte == b'-' || byte == b'.' || byte == b':' || byte == b'/';
+        let new_alnum_state = (byte as char).is_alphanumeric() || byte == b'-' || byte == b'.' || byte == b':' || byte == b'/' || byte == b'#' || byte == b'?' || byte == b'&';
 
         if alnum_state != new_alnum_state || !new_alnum_state {
             if let Ok(s) = String::from_utf8(s) {

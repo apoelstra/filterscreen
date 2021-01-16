886a1c22e307c966689a39b47fde498121fc999f
Andrew Poelstra <apoelstra@wpsoftware.net>
2021-01-16 18:32:13 +0000

    add a ton more shit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
-use bitcoin;
-use bitcoin::hashes::{sha256, hash160};
-use bitcoin::hashes::hex::FromHex;
-use bitcoin::consensus::Decodable;
+use miniscript::{self, bitcoin};
+use miniscript::bitcoin::hashes::{sha256, hash160};
+use miniscript::bitcoin::hashes::hex::FromHex;
+use miniscript::bitcoin::consensus::Decodable;
 use std::collections::BTreeSet;
 use std::io::{self, Read};
 use std::str::FromStr;
@@ -17,6 +17,10 @@ fn main() -> io::Result<()> {
 
     // Split in various ways
     let words = full_string.split(&[' ', '\t', '\n'][..]);
+
+    let hex_decoded = words
+        .clone()
+        .filter_map(|word| Vec::<u8>::from_hex(word).ok().map(|vec| (vec, word)));
 
     let alnum_words = words
         .clone()
@@ -33,6 +37,7 @@ fn main() -> io::Result<()> {
 
     let nums = words
         .clone()
+        .filter(|word| word.len() > 3) // short numbers we don't need to copy/paste
         .filter(|word| f64::from_str(word).is_ok());
 
     let mut results = BTreeSet::new();
@@ -46,6 +51,20 @@ fn main() -> io::Result<()> {
         }
     }
 
+    for (dehex, word) in hex_decoded {
+        if bitcoin::Transaction::consensus_decode(&*dehex).is_ok() {
+            results.insert(format!("tx: {}", word));
+        } else if bitcoin::TxOut::consensus_decode(&*dehex).is_ok() {
+            results.insert(format!("txout: {}", word));
+        } else if bitcoin::TxIn::consensus_decode(&*dehex).is_ok() {
+            results.insert(format!("txin: {}", word));
+        } else if bitcoin::BlockHeader::consensus_decode(&*dehex).is_ok() {
+            results.insert(format!("header: {}", word));
+        } else if bitcoin::Block::consensus_decode(&*dehex).is_ok() {
+            results.insert(format!("block: {}", word));
+        }
+    }
+
     for word in alnum_words {
         if sha256::Hash::from_str(&word).is_ok() {
             results.insert(format!("hash32: {}", word));
@@ -53,6 +72,16 @@ fn main() -> io::Result<()> {
             results.insert(format!("hash20: {}", word));
         } else if bitcoin::Address::from_str(&word).is_ok() {
             results.insert(format!("address: {}", word));
+        } else if bitcoin::PublicKey::from_str(&word).is_ok() {
+            results.insert(format!("pubkey: {}", word));
+        } else if bitcoin::PrivateKey::from_str(&word).is_ok() {
+            results.insert(format!("privkey: {}", word));
+        } else if bitcoin::util::bip32::ExtendedPubKey::from_str(&word).is_ok() {
+            results.insert(format!("xpub: {}", word));
+        } else if bitcoin::util::bip32::ExtendedPrivKey::from_str(&word).is_ok() {
+            results.insert(format!("xpriv: {}", word));
+        } else if miniscript::Descriptor::<String>::from_str(&word).is_ok() {
+            results.insert(format!("desc: {}", word));
         }
     }
 

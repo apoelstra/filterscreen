a10eada8da8e0c34e1e8a130389b51210006aac7
Andrew Poelstra <apoelstra@wpsoftware.net>
2021-01-16 18:22:53 +0000

    overhaul

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,74 @@ use bitcoin;
 use bitcoin::hashes::{sha256, hash160};
 use bitcoin::hashes::hex::FromHex;
 use bitcoin::consensus::Decodable;
+use std::collections::BTreeSet;
 use std::io::{self, Read};
 use std::str::FromStr;
 
 fn main() -> io::Result<()> {
-    let mut words = vec![];
+    // Capture screen
+    let full_string = {
+        let mut s = String::new();
+        io::stdin().read_to_string(&mut s)?;
+        s
+    };
 
-    let mut s = vec![];
-    let mut alnum_state = false;
-    for byte in io::stdin().bytes() {
-        let byte = byte?;
-        let new_alnum_state = (byte as char).is_alphanumeric() || byte == b'-' || byte == b'.' || byte == b':' || byte == b'/' || byte == b'#' || byte == b'?' || byte == b'&';
+    // Split in various ways
+    let words = full_string.split(&[' ', '\t', '\n'][..]);
 
-        if alnum_state != new_alnum_state || !new_alnum_state {
-            if let Ok(s) = String::from_utf8(s) {
-                words.push(s);
-            }
-            s = vec![];
-        }
-        s.push(byte);
+    let alnum_words = words
+        .clone()
+        .filter(|word| word.chars().all(char::is_alphanumeric));
 
-        alnum_state = new_alnum_state;
-    }
+    let ips = words
+        .clone()
+        .filter(|word| std::net::IpAddr::from_str(word).is_ok());
 
-    words.sort();
-    words.dedup();
+    let macs = words
+        .clone()
+        .filter(|word| word.chars().all(|ch| ch.is_alphabetic() || ch == ':') &&
+            word.chars().filter(|ch| *ch == ':').count() == 5);
 
-    // Try to parse words as various thingsha256
+    let nums = words
+        .clone()
+        .filter(|word| f64::from_str(word).is_ok());
+
+    let mut results = BTreeSet::new();
     for word in words {
-        if sha256::Hash::from_str(&word).is_ok() {
-            println!("hash32: {}", word);
-        } else if hash160::Hash::from_str(&word).is_ok() {
-            println!("hash20: {}", word);
-        } else if bitcoin::Address::from_str(&word).is_ok() {
-            println!("address: {}", word);
-        } else if url::Url::from_str(&word).is_ok() {
+        if url::Url::from_str(&word).is_ok() {
             if word.len() > 7 {
                 if &word[..8] == "https://" || &word[..7] == "http://" || &word[..4] == "www." {
-                    println!("url: {}", word);
+                    results.insert(format!("url: {}", word));
                 }
             }
-        } else if (u64::from_str(&word).is_ok() && word.len() > 4)
-            || (f64::from_str(&word).is_ok() && word.len() > 4) {
-            println!("num: {}", word);
-        } else if let Ok(hex) = Vec::<u8>::from_hex(&word) {
-            if bitcoin::Transaction::consensus_decode(&*hex).is_ok() {
-                println!("tx: {}", word);
-            }
         }
+    }
+
+    for word in alnum_words {
+        if sha256::Hash::from_str(&word).is_ok() {
+            results.insert(format!("hash32: {}", word));
+        } else if hash160::Hash::from_str(&word).is_ok() {
+            results.insert(format!("hash20: {}", word));
+        } else if bitcoin::Address::from_str(&word).is_ok() {
+            results.insert(format!("address: {}", word));
+        }
+    }
+
+    for word in ips {
+        results.insert(format!("ip: {}", word));
+    }
+
+    for word in macs {
+        results.insert(format!("mac: {}", word));
+    }
+
+    for word in nums {
+        results.insert(format!("num: {}", word));
+    }
+
+    // Output
+    for line in results {
+        println!("{}", line);
     }
 
     Ok(())
